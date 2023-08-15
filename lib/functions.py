2937669c579ca3fba778e743de0ac3ab8254341d
samuel <samuel.mbula@student.moringaschool.com>
2023-08-15 12:13:36 +0300

    python-p3-functions

--- lib/functions.py
+++ lib/functions.py
@@ -3,13 +3,15 @@
 def greet_programmer():
     print("Hello, programmer!")
 def greet(name):
-    print("Hello, {}".format(name))
-def greet_with_default(name=None):
-    if name is None:
-        print("Hello, programmer!")
-    else:
-        print("Hello, {}".format(name))
-def add(a, b):
-    return a + b
-def halve(n):
-    return n / 2
+    name="Guido"
+    print(f"Hello, {name}!")
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+def add(num1, num2):
+    return num1+num2
+num1 =45    
+num2 =55
+def halve (number):
+    return number/2
+number =100
+    
